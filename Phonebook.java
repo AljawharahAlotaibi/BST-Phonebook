a4194a5709aea32ddbe0a60e8bc0447ca18da369
Layan59 <112674738+Layan59@users.noreply.github.com>
2023-10-07 07:40:03 +0300

    Update Phonebook.java

--- Phonebook.java
+++ Phonebook.java
@@ -9,10 +9,12 @@
 // that have these values
 
 import java.util.Scanner;
+
 public class Phonebook {
+    DoubleLinkedList<Contact> field;
     public static void main(String[]args)
     {
-Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Welcome to the Linked Tree Phonebook!\r\n" + //
                 "Please choose an option:\r\n" + //
                 "1. Add a contact\r\n" + //
@@ -25,17 +27,41 @@ Scanner input = new Scanner(System.in);
                 "8. Exit");
 
                 System.out.println("Enter your choice: ");
+
                 int choice = input.nextInt(); 
-        
+
+
                 switch(choice){
 
                 case 1:
+    DoubleLinkedList<Contact> contactList = new DoubleLinkedList<>();
+    // Create some test contacts
+    Contact contact1 = new Contact("John Doe", "1234567890", "john.doe@example.com", "123 Main St", "1990-01-01", "Some notes about John");
+    Contact contact2 = new Contact("Saud moh", "9876543210", "jane.smith@example.com", "456 Elm St", "1985-05-10", "Some notes about Jane");
+    Contact contact3 = new Contact("Alice Johnson", "9876543210", "alice.johnson@example.com", "789 Oak St", "1998-12-25", "Some notes about Alice");
+    Contact contact4 = new Contact("John Doe", "1111111111", "bob.williams@example.com", "321 Pine St", "1977-09-15", "Some notes about Bob");
+
+    // Test the addElement method
+    contactList.addContact(contact1);
+    contactList.addContact(contact2);
+    contactList.addContact(contact3);
+    contactList.addContact(contact4);
+
+    // Print the contents of the contact list
+    //contactList.printList();
+
                 break;
 
                 case 2:
                 break;
 
                 case 3:
+                System.out.println("Enter name: ");
+                String name = input.nextLine();
+                contactList.remove(name);
+                //test example
+                contactList.remove("Alice Johnson");
+                
                 break;
 
                 case 4:
@@ -53,28 +79,12 @@ Scanner input = new Scanner(System.in);
                 case 8:
                 break;
 
+
+
                 }
-        
-    DoubleLinkedList<Contact> contactList = new DoubleLinkedList<>();
-    // Create some test contacts
-    Contact contact1 = new Contact("John Doe", "1234567890", "john.doe@example.com", "123 Main St", "1990-01-01", "Some notes about John");
-    Contact contact2 = new Contact("Saud moh", "9876543210", "jane.smith@example.com", "456 Elm St", "1985-05-10", "Some notes about Jane");
-    Contact contact3 = new Contact("Alice Johnson", "9876543210", "alice.johnson@example.com", "789 Oak St", "1998-12-25", "Some notes about Alice");
-    Contact contact4 = new Contact("John Doe", "1111111111", "bob.williams@example.com", "321 Pine St", "1977-09-15", "Some notes about Bob");
+    
 
-    // Test the addElement method
-    contactList.addElement(contact1);
-    contactList.addElement(contact2);
-    contactList.addElement(contact3);
-    contactList.addElement(contact4);
-
-    // Print the contents of the contact list
-    contactList.printList();
-
-    }
-
-
-
+    
 
 //printing all contacts that share an event as well as all
 // contacts that share the first name.
@@ -87,4 +97,6 @@ Scanner input = new Scanner(System.in);
     {
         
     }
+}
+
 }
