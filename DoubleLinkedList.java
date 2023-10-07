cea040689013e116ffd9a10b56bc0ae1358c6400
Layan59 <112674738+Layan59@users.noreply.github.com>
2023-10-07 08:02:54 +0300

    Update DoubleLinkedList.java

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,5 +1,6 @@
+import javax.swing.text.html.parser.Element;
 
-public class DoubleLinkedList<T> {
+public class DoubleLinkedList<T> implements List{
     private Node<T> head; 
     private Node<T> current;
 
@@ -13,122 +14,70 @@ public class DoubleLinkedList<T> {
         return head==null;
     }
 
-    // public boolean last()
-    // {
-    //     return current.next==null;
-    // }
+    public boolean last()
+    {
+        return current.next==null;
+    }
 
-    // public boolean first ()
-    // {
-    //     return current.previous==null;
-    // }
+    public boolean first ()
+    {
+        return current.previous==null;
+    }
 
-    // public boolean full()
-    // {
-    //     return false;
-    // }
+    public boolean full()
+    {
+        return false;
+    }
     public void findFirst ()
     {
         current=head;
     }
-    // public void findNext ()
-    // {
-    //     current=current.next;
-    // }
-    // public void findPrevious()
-    // {
-    //     current=current.previous;
-    // }
+    public void findNext ()
+    {
+        current=current.next;
+    }
+    public void findPrevious()
+    {
+        current=current.previous;
+    }
 
-    // public T retrieve()
-    // {
-    //     return current.data;
-    // }
-    // public void update(T val)
-    // {
-    //     current.data=val;
-    // }
-    // Each contact in the phonebook should be unique. You can ensure this by checking if a contact with
-    // the same name or phone number already exists in the list before adding a new contact.
+    public T retrieve()
+    {
+        return current.data;
+    }
+    public void update(T val)
+    {
+        current.data=val;
+    }
+//adding contact need to reWrite
+// Each contact in the phonebook should be unique. You can ensure this by checking if a contact with
+// the same name or phone number already exists in the list before adding a new contact.
 
-    public void addElement(T element){
-        if(element instanceof Contact){
-            addContact((element));
-            return;
-        }
-        else if(element instanceof Event)
+    public void insert(T element){
+        if(element instanceof Contact)
+            if(!addContact(element)){
+                System.out.println("contact already exixsts");
+                return;
+            }
+        if(element instanceof Event)
             if(!addEvent(element)){
                 System.out.println("there is conflict with a current schedueld event");                
                 return;
             }
-    }
-    
-    public void addContact(T contact) {
-        Node<T> tmp = new Node<>(contact);
+            }
 
-        // If the list is empty
-        if (empty()) {
-            head = current = tmp;
-            System.out.println("Contact added: " + ((Contact) contact).getContactName());
-            return;
-        }
-    
-        // Check if the contact already exists in the list 
-        findFirst();
-        while (current != null) {
-            if (((Contact) contact).getContactName().equalsIgnoreCase(((Contact) current.getData()).getContactName()) ||
-                    ((Contact) contact).getPhoneNumber().equals(((Contact) current.getData()).getPhoneNumber())) {
-                System.out.println("Contact already exists: " + ((Contact) contact).getContactName());
-                return;
-            }
-            current = current.next;
-        }
-    
-        // If the new contact should be inserted at the beginning of the list
-        if (((Contact) contact).compareTo(head.getData()) < 0) {
-            tmp.next = head;
-            head.previous = tmp;
-            head = tmp;
-            System.out.println("Contact added: " + ((Contact) contact).getContactName());
-            return;
-        }
-    
-        findFirst();
-        while (current != null) {
-            // If the new contact should be inserted in the middle of the list
-            if (((Contact) contact).compareTo(current.getData()) < 0) {
-                tmp.next = current;
-                tmp.previous = current.previous;
-                current.previous.next = tmp;
-                current.previous = tmp;
-                System.out.println("Contact added: " + ((Contact) contact).getContactName());
-                return;
-            }
-    
-            // If we reach the end of the list, insert the new contact at the end
-            if (current.next == null) {
-                current.next = tmp;
-                tmp.previous = current;
-                System.out.println("Contact added: " + ((Contact) contact).getContactName());
-                return;
-            }
-            current = current.next;
-        }
-    }
-
-public boolean addEvent(T event)
-    {
-        //check schedule
-        if(!true);
-        // event.d<Contact>.getDate();
-        return false;
-    }
 
 //searching contact
 // When you search for a contact by email address, address, or birthday, you should return all contacts
 // that have these values
+
 // the search for an event is based on the event title or contact name
-     public DoubleLinkedList<Contact> searchContact(String searchchoice){
+//searching contact
+// When you search for a contact by email address, address, or birthday, you should return all contacts
+// that have these values
+// the search for an event is based on the event title or contact name
+
+public DoubleLinkedList<Contact> searchContact(String searchchoice){
     DoubleLinkedList<Contact> resultList = new DoubleLinkedList<Contact>();
     findFirst();
     while(current!=null){
@@ -142,9 +91,8 @@ public boolean addEvent(T event)
     }
 
 
-    //Layan tst
+    
     //deleting contact need to reWrite
-     //deleting contact need to reWrite
     public void remove(String name){
         //When a contact is deleted all events with that contact are also deleted
       current = head ;
